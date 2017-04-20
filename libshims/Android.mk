12aa1fa94f6e71fac7c2ddcc2e5128007c11a458
grace5921 <androidlover5842@gmail.com>
2017-04-20 13:18:53 +0530

    msm8916: add shims to load libwvm

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -28,3 +28,11 @@ LOCAL_SRC_FILES := libshim_camera.cpp
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_SRC_FILES := libshim_wvm.cpp
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
---
+++ libshims/libshim_wvm.cpp
@@ -0,0 +1,2 @@
+#include <stdlib.h>
+extern "C" void _ZN7android16MediaBufferGroupC1Ev(){}
